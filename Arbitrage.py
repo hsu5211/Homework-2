13d528e59269cf9b09681c4d57e1c68f4a5329c2
hsu5211 <141152413+hsu5211@users.noreply.github.com>
2024-04-12 21:28:32 +0800

    Update Arbitrage.py

--- Arbitrage.py
+++ Arbitrage.py
@@ -10,3 +10,36 @@ liquidity = {
     ("tokenC", "tokenE"): (10, 8),
     ("tokenD", "tokenE"): (60, 25),
 }
+
+Tok = ["tokenA", "tokenB", "tokenC", "tokenD", "tokenE"]
+
+def Swap(Amt, From, To) :
+    if (From, To) not in liquidity:
+        F,T = liquidity[(To, From)]
+        return F-T*F/(T+Amt)
+    else:
+        F,T = liquidity[(From, To)]
+        return T-T*F/(F+Amt)
+
+def FindPath(CurTok, CurAmt, Path, Visited, MaxAmt) :
+    if len(Path) > 1 and Path[-1] == "tokenB" and CurAmt > MaxAmt["amount"]:
+        MaxAmt["amount"] = CurAmt
+        MaxAmt["path"] = Path.copy()
+    for Token in Tok:
+        if Token != CurTok:
+            if (CurTok, Token) in liquidity :
+                edge = (CurTok, Token)
+            else :
+                edge = (Token, CurTok)
+            if edge not in Visited:
+                NewAmt = Swap(CurAmt, CurTok, Token)
+                Visited.add(edge)
+                Path.append(Token)
+                FindPath(Token, NewAmt, Path, Visited, MaxAmt)
+                Path.pop()
+                Visited.remove(edge)
+    return
+
+MaxAmt = {"amount": 0, "path": []}
+FindPath("tokenB", 5, ["tokenB"], set(), MaxAmt)
+print("path: ", "->".join(MaxAmt['path']), ", ", f"tokenB balance={MaxAmt['amount']:.6f}", sep="")
